7cc084c256fa7c5cbddd913c02dda9151b4b2eba
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2025-11-27 15:38:47 +0700

    May moi ahihi

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -12,12 +12,8 @@ import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
-
-
-
-
 @Controller
-public class UserController{
+public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
 
@@ -27,33 +23,31 @@ public class UserController{
     }
 
     @RequestMapping("/")
-    public String getHomePage(){
+    public String getHomePage() {
         String test = this.userService.handleHello();
         return "hello";
     }
-    
+
     @RequestMapping("/admin/user/create")
-    public String getUserCreationPage(Model model){
+    public String getUserCreationPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
-    
-    @RequestMapping(value="/admin/user", method=RequestMethod.GET)
-    public String getUserPage(Model model){
+
+    @RequestMapping(value = "/admin/user", method = RequestMethod.GET)
+    public String getUserPage(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users1", users);
         return "admin/user/table-user";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User user){
-        System.out.println("User: "+user);
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("User: " + user);
         this.userRepository.save(user);
         return "redirect:/admin/user";
     }
-    
 
+    // Yeah
 
-    
 }
-
