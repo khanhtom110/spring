3350c0f214f322066606c2c8eff90e5ebf22b4b9
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2025-11-28 22:46:39 +0700

    legging day

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -48,6 +49,11 @@ public class UserController {
         return "redirect:/admin/user";
     }
 
-    // Yeah
+    @RequestMapping(value = "/admin/user/{id}", method = RequestMethod.GET)
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users1", users);
+        return "admin/user/show";
+    }
 
 }
